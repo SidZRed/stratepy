0f102459847f3cda561a0a2ef722d350ea557732
Siddharth Reddy <sidzred@gmail.com>
2024-03-21 21:12:16 +0530

    Included setup.py for setting up to publish as package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stratepy',
+    version='0.1',
+    packages=find_packages(),
+    description='A python package for testing and experimenting with strategies related to Game theory, voting systems and auction systems',
+    author='Siddharth Reddy',
+    author_email='siddharthrr@iisc.ac.in',
+    url='https://github.com/SidZRed/stratepy',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Operating System :: OS Independent',
+    ],
+)
